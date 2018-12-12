f75b31cd9458a40332fe9fd9093e3f30d8a55190
Mikhail Krylov <2903603@gmail.com>
2018-12-12 19:56:59 +0300

    Добавили проект Unity

---
+++ TaskGit /Assets/Creepy_Cat/3D Scifi/Scripts/AnimationСontroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationСontroller : MonoBehaviour
+{
+   
+    public string triggerToSet;
+    private Animator anim;
+    private void Start()
+   
+
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    public void SetTrigger()
+
+    {
+        anim.SetTrigger(triggerToSet);
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+
+   
+}
